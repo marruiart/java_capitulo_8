86592e59b6bb86041ee732f6218f5b4439844698
Marina Ruiz <marruiart@gmail.com>
2022-11-04 13:32:45 +0100

    exercise 16 added

---
+++ Ejercicio16.java
@@ -0,0 +1,18 @@
+import maths.General;
+
+/**
+ * Este programa muestra los números capicúa que hay entre 1 y 99999
+ *
+ * @author: Marina Ruiz Artacho
+ **/
+
+public class Ejercicio16 {
+    public static void main(String[] args) {
+        int min = 1;
+        int max = 99999;
+
+        for (int i = min; i <= max; i++)
+            if (General.isPalindromic(i))
+                System.out.printf("%d ", i);
+    }
+}
