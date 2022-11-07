a85e2cb41a52ec4a455a8a1951938109752fd9b1
Marina Ruiz <marruiart@gmail.com>
2022-11-07 14:17:14 +0100

    add exercise 29

---
+++ arrays/Bidimensional.java
@@ -0,0 +1,34 @@
+package arrays;
+
+/**
+ * Biblioteca de funciones para arrays bidimensionales (de dos dimensiones) de
+ * números enteros.
+ *
+ * @author: Marina Ruiz Artacho
+ **/
+
+public class Bidimensional {
+
+    /**
+     * Genera un array de tamaño n con números aleatorios cuyo intervalo (mínimo y
+     * máxmo) se indica como parámetro.
+     * 
+     * @param n   número de filas del array
+     * @param m   número de columnas del array
+     * @param max valor máximo del intervalo de números aleatorios
+     * @param min valor mínimo del intervalo de números aleatorios
+     * @return array de tamaño n relleno de números aleatorios. Devuelve un array
+     *         vacío en caso de error
+     **/
+
+    public static int[][] createArrayBiInt(int n, int m, int min, int max) {
+        int[][] array = new int[n][m];
+        if (min >= max)
+            return array;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                array[i][j] = min + (int) (Math.random() * ((max - min) + 1));
+        return array;
+    }
+
+}
