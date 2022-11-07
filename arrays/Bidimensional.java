da8ba2a0f852310b410567fd4825f70036a81fb4
Marina Ruiz <marruiart@gmail.com>
2022-11-07 14:25:52 +0100

    add exercise 30

--- arrays/Bidimensional.java
+++ arrays/Bidimensional.java
@@ -31,4 +31,22 @@ public class Bidimensional {
         return array;
     }
 
+    /**
+     * Devuelve la fila i-ésima del array que se pasa como parámetro.
+     * 
+     * @param n   número de filas del array
+     * @param m   número de columnas del array
+     * @param max valor máximo del intervalo de números aleatorios
+     * @param min valor mínimo del intervalo de números aleatorios
+     * @return array de tamaño n relleno de números aleatorios. Devuelve un array
+     *         vacío en caso de error
+     **/
+
+    public static int[] rowOfArrayBiInt(int array[][], int i) {
+        int[] row = new int[array[i].length];
+        for (int j = 0; j < array[i].length; j++)
+            row[j] = array[i][j];
+        return row;
+    }
+
 }
